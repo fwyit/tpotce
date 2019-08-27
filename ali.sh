f0bb0279bf204e049a78280dd27e5360048a52f2
Jam <jianliu@newbanker.cn>
2019-08-27 15:57:44 +0800

    add 新增docker-compose程序

--- ali.sh
+++ ali.sh
@@ -6,7 +6,7 @@
 
 cd iso/installer
 
-test -z "$(which git 2>/dev/null)" && apt-get update && apt install -y git
+test -z "$(which git 2>/dev/null)" && apt-get update && apt install -y git docker-compose
 test ! -d /opt/tpot && git clone -b dev https://github.com/fwyit/tpotce /opt/tpot
 MIRROR=registry-vpc.cn-beijing.aliyuncs.com
 curl -m 2 https://$MIRROR/v2/ || MIRROR=registry.cn-beijing.aliyuncs.com
