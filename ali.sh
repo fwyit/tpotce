74b4abce507719ce9c33e1d1e96fd7dd6654444b
Jam <jianliu@newbanker.cn>
2019-08-27 17:08:31 +0800

    opt 优化使用阿里云下载方式

--- ali.sh
+++ ali.sh
@@ -4,12 +4,27 @@
 #description : 本脚本主要用来使用阿里云镜像加速国内下载
 
 
-cd iso/installer
 
 test -z "$(which git 2>/dev/null)" && apt-get update && apt install -y git docker-compose
-test ! -d /opt/tpot && git clone -b dev https://github.com/fwyit/tpotce /opt/tpot
 MIRROR=registry-vpc.cn-beijing.aliyuncs.com
 curl -m 2 https://$MIRROR/v2/ || MIRROR=registry.cn-beijing.aliyuncs.com
+if test -z "$(which docker 2>/dev/null)"; then
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    echo "deb https://mirrors.aliyuncs.com/docker-ce/linux/debian/ `lsb_release -cs` stable" > /etc/apt/sources.list.d/docker.list
+    apt-get update && apt-get install -y docker-ce
+fi
+
+repo=$MIRROR/fwy/tpot
+docker pull $repo:src
+docker run -d --name tpot $repo:src tail -f /etc/hosts
+if test ! -d /opt/tpot; then
+    mkdir -p /opt/tpot
+    docker cp tpot:/src.tgz /tmp/
+    tar -zxf /tmp/src.tgz -C /opt/tpot
+    docker stop tpot
+    docker rm tpot
+fi
+#test ! -d /opt/tpot && git clone -b dev https://github.com/fwyit/tpotce /opt/tpot
 sed -i -e "s@dtagdevsec/\(.*\):.*\"@$MIRROR/fwy/tpot:\1\"@g" /opt/tpot/etc/compose/*
 
-./install.sh "$@"
+bash /opt/tpot/iso/installer/install.sh "$@"
